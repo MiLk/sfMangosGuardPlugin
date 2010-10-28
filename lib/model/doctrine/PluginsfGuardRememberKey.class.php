28ff654f094fc81ac4b03e164bff53e8cabe6f09
MiLk <milk@world-of-ysera.com>
2010-10-28 18:03:59 +0200

    Add relation to model, beacause they aren't supported by MyISAM.
    Signed-off-by: MiLk <milk@world-of-ysera.com>

--- lib/model/doctrine/PluginsfGuardRememberKey.class.php
+++ lib/model/doctrine/PluginsfGuardRememberKey.class.php
@@ -2,6 +2,11 @@
 
 /**
  * Remember me model.
+ * 
+ * @property Account $User
+ *
+ * @method Account            getUser()         Returns the current record's "User" value
+ * @method sfGuardRememberKey setUser()         Sets the current record's "User" value
  *
  * @package    sfDoctrineGuardPlugin
  * @subpackage model
@@ -10,4 +15,14 @@
  */
 abstract class PluginsfGuardRememberKey extends BasesfGuardRememberKey
 {
+
+  public function setUp()
+  {
+    parent::setUp();
+    $this->hasOne('Account as User',array(
+     'local' => 'user_id',
+     'foreign' => 'id',
+     'onDelete' => 'CASCADE'));
+  }
+
 }
